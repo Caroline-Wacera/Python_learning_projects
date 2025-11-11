a87100f818cc42de6cfe79b952df1282ed074758
Caroline-Wacera <carolinewacera4@gmail.com>
2025-11-12 00:06:49 +0300

    Add Day 1 personal_info project

---
+++ personal_info.py
@@ -0,0 +1,15 @@
+#Practice – 5 Quick Exercises
+#Ask the user for their name and print it
+name = input("Enter your name:")
+print(f"My name is {name}")
+#Ask the user for their age and print "You are <age> years old"
+age = int(input("Enter your age:"))
+print(f"You are {age} years old")
+#Ask the user for their city and print "You live in <city>"
+city = input("Enter the city you live in:")
+print(f"You live in {city}")
+#Combine all three into one sentence using f-string
+print(f"My name is {name}, I am {age} years old and i live in {city}.")
+#Ask for favorite color and print "Your favorite color is <color>
+favorite_color = input("Enter your favorite color:")
+print(f"Your favorite color is {favorite_color}")
